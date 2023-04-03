65a56a58a7952dc8017c196abb97aa5287aa7a49
Edwards Fonseca <eduards_fonseca@hotmail.com>
2023-04-02 21:09:26 -0400

    agregando estilos la navb,agregando cards ,imgs,fondo, part 1}

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import Navbar from './navbar'
+import Landpage from './landpage'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Navbar />
+    <Landpage/>
   </React.StrictMode>,
 )
--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import "./navbar.css"
+import Landpage from './landpage'
 const Navbar = () => {
   return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg bg">
   <div class="container-fluid">
     {/* <a class="navbar-brand" href="#">Navbar</a> */}
     <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -11,16 +12,16 @@ const Navbar = () => {
     <div class="collapse navbar-collapse navbar-style" id="navbarNavDropdown">
       <ul class="navbar-nav buttonf">
         <li class="nav-item ">
-          <a class="nav-link" aria-current="page" href="#">HOME</a>
+          <a class="nav-link" aria-current="page" href="#Home">HOME</a>
         </li>
         <li class="nav-item ">
-          <a class="nav-link" href="#">MENU</a>
+          <a class="nav-link" href="#Menu">MENU</a>
         </li>
         <li class="nav-item ">
-          <a class="nav-link" href="#">PLANES</a>
+          <a class="nav-link" href="#Planes">PLANES</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link " href="#">CONTACTO</a>
+          <a class="nav-link " href="#Contacto">CONTACTO</a>
         </li>
         
       </ul>
